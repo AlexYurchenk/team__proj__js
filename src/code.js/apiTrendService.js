aa95fcadb0d56239191a29d58bab56f2243d8c43
AlexYurchenk <75500247+AlexYurchenk@users.noreply.github.com>
2021-06-18 14:27:07 +0300

    ми

--- src/code.js/apiTrendService.js
+++ src/code.js/apiTrendService.js
@@ -16,6 +16,7 @@ function fetchtrend() {
   
 })
 .then(({ results }) => {
+    console.log(results)
     return results;
 })
 }
--- src/code.js/main.js
+++ src/code.js/main.js
@@ -1,4 +1,3 @@
-
 import SearchApiTrend from "./apiTrendService.js";
 
 import trendMovieTpl from '../templates/trendfilm-cards.hbs';
@@ -13,24 +12,27 @@ SearchApiTrend.fetchtrend().then(results => {
 
 function renderMovies(results) {
     // console.log(results);
-    const markUp = trendMovieTpl(results);
-    fetchGenres();
-    refs.trendContainer.insertAdjacentHTML('beforeend',markUp);
+    fetchGenres()
+    .then( a => {
+        let temp = {};
+        for(let e of a){
+            temp[e.id] = e.name;
+        }
+        for(let i = 0; i < results.length; i++){
+            results[i].genre_ids = results[i].genre_ids.map( gen => temp[gen])
+        }
+
+        const markUp = trendMovieTpl(results);
+        refs.trendContainer.insertAdjacentHTML('beforeend',markUp);
+    })
 }
 
 function fetchGenres() {
-    fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=61153224aaaa08b03f5d3b14add082d2&language=en-US')
+   return fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=61153224aaaa08b03f5d3b14add082d2&language=en-US%27')
     .then(r => r.json())
-    .then(({ genres }) => genres )
+    .then(({ genres }) => {
+        console.log(genres);
+        return genres;
+    })
 }
-
-console.log(trendMovieTpl)
-
-
-
-
-
-
-
-
 
