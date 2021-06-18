853148ef41c726807d971bc527855c1e06f310c7
viacheslavkrupnov <slava86krupnov@gmail.com>
2021-06-18 19:51:32 +0400

    lightbox dependency

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import './sass/main.scss';
+import * as basicLightbox from 'basiclightbox';
+import 'basiclightbox/dist/basicLightbox.min.css';
 import './code.js/main';
+import './code.js/modal';
 
