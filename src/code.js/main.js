a5080b3423f29d624891190ced9d67d5d0444f14
Leskovets Maksym <leskovecmaksim@gmail.com>
2021-06-21 21:49:54 +0200

    add

--- src/code.js/main.js
+++ src/code.js/main.js
@@ -13,6 +13,8 @@ const refs = {
     closeModalBtn: document.querySelector('[data-action="close-lightbox"]'),
     overlayModal: document.querySelector('.modal-movie-overlay'),
 
+    btnAddWatched: document.querySelector('[data-name="watched"]'),
+    btnAddQueue: document.querySelector('[data-name="queue"]'),
 }
 
 SearchApiTrend.fetchtrend().then(results => {
@@ -48,26 +50,64 @@ function fetchGenres() {
         return temp;
     })
 }
-refs.trendContainer.addEventListener('click',e => {
-    if(e.target.nodeName !=='IMG'){
+refs.trendContainer.addEventListener('click', onModalFilmCard)
+
+function onModalFilmCard(e) {
+    if (e.target.nodeName !== 'IMG') {
         return
-      }
+    }
     const a = e.target.id
     return fetch(`https://api.themoviedb.org/3/movie/${a}?api_key=44d74a10460e9a32f8546bed31d47780&language=en-US`)
-    .then(r => r.json())
-    .then( film => {
-        console.log(film)
-        console.log(film.id)
-        return film
-    })
-    .then(film => {
-        const markUp = articleTpl(film);
+        .then(r => r.json())
+        .then(film => {
+            console.log(film)
 
-        refs.lightbox.classList.toggle('modal-is-open')
-        refs.overlayModal.insertAdjacentHTML('beforeend',markUp)
+            refs.btnAddWatched.addEventListener('click', e => {
+                const watched = JSON.parse(localStorage.getItem('watched')) || [];
+                localStorage.setItem('watched', JSON.stringify(watched))
+                
+                if (localStorage.getItem('watched').includes(JSON.stringify(film.id))) {
+                    return
+                }
+                watched.push(film.id)
+                console.log(watched)
+                localStorage.setItem('watched', JSON.stringify(watched))
+
+            })
+
+            refs.btnAddQueue.addEventListener('click', e => {
+                const queue = JSON.parse(localStorage.getItem('queue')) || [];
+                localStorage.setItem('queue', JSON.stringify(queue))
+
+                if(localStorage.getItem('queue').includes(JSON.stringify(film.id))) {
+                    return
+                }
+                queue.push(film.id);
+                console.log(queue)
+                localStorage.setItem('queue', JSON.stringify(queue))
+            })
+
+            return film
+        })
+        .then(film => {
+            const markUp = articleTpl(film);
+
+            refs.lightbox.classList.toggle('modal-is-open')
+            refs.overlayModal.insertAdjacentHTML('beforeend', markUp)
         
-    })
-})
+        })
+}
+
+function fetchMovie() {
+    fetch(`https://api.themoviedb.org/3/movie/${a}?api_key=44d74a10460e9a32f8546bed31d47780&language=en-US`)
+        .then(r => r.json())
+        .then(film => {
+            console.log(film)
+            console.log(film.id)
+            return film
+        })
+}
+
 refs.closeModalBtn.addEventListener('click',onBtnClose)
 function onBtnClose(){
 
