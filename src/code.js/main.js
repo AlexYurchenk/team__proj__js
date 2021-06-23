d5204308d76939109813e1cb71310771711ec9b7
Kate <kamennaya.kate@gmail.com>
2021-06-23 12:42:07 +0300

    updated
    -удалила строки в пагинации
    - внесла изменения по footer от Кати Ю.

--- src/code.js/main.js
+++ src/code.js/main.js
@@ -6,20 +6,20 @@ import articleTpl from '../templates/modal-card.hbs'
 const refs = {
     trendContainer: document.querySelector('.js-trend-list'),
 
-    button:document.querySelector('.btn-list'),
+    button: document.querySelector('.btn-list'),
 
     modal: document.querySelector('.modal'),
     lightbox: document.querySelector('.modal-movie-lightbox'),
     closeModalBtn: document.querySelector('[data-action="close-lightbox"]'),
-    overlay:document.querySelector('.modal-movie-lightbox'),
+    overlay: document.querySelector('.modal-movie-lightbox'),
     overlayModal: document.querySelector('.js-modal-movie-overlay'),
-    next:document.querySelector('.js-btn-next'),
+    next: document.querySelector('.js-btn-next'),
     pr: document.querySelector('.js-btn-pr'),
-    btnList : document.querySelector('.button-list__container'),
+    btnList: document.querySelector('.button-list__container'),
     btnListPage: document.querySelector('.button-list__page'),
     wrapperEl: document.querySelector('.modal-movie-wrapper'),
     poster: document.querySelector('.card-poster-wrapper'),
-    information: document.querySelector('.modal-movie-information')
+    information: document.querySelector('.modal-movie-information'),
 
     btnAddWatched: document.querySelector('[data-name="watched"]'),
     btnAddQueue: document.querySelector('[data-name="queue"]'),
@@ -34,31 +34,31 @@ SearchApiTrend.fetchtrend().then(results => {
 function renderMovies(results) {
     // console.log(results);
     fetchGenres()
-    .then( genres => {
+        .then(genres => {
 
-        results.forEach(result => {
-            result.genre_ids = result.genre_ids.map( genre => genres[genre])
-            result.release_date = result.release_date.slice(0,4)
-        });
+            results.forEach(result => {
+                result.genre_ids = result.genre_ids.map(genre => genres[genre])
+                result.release_date = result.release_date.slice(0, 4)
+            });
 
 
-        const markUp = trendMovieTpl(results);
-        refs.trendContainer.insertAdjacentHTML('beforeend',markUp);
-        refs.button.classList.add('none');
-    })
+            const markUp = trendMovieTpl(results);
+            refs.trendContainer.insertAdjacentHTML('beforeend', markUp);
+            refs.button.classList.add('none');
+        })
 
 }
 
 function fetchGenres() {
-   return fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=61153224aaaa08b03f5d3b14add082d2&language=en-US%27')
-    .then(r => r.json())
-    .then(({ genres }) => {
-        let temp = {};
-        for(let genre of genres){
-            temp[genre.id] = genre.name;
-        };
-        return temp;
-    })
+    return fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=61153224aaaa08b03f5d3b14add082d2&language=en-US%27')
+        .then(r => r.json())
+        .then(({ genres }) => {
+            let temp = {};
+            for (let genre of genres) {
+                temp[genre.id] = genre.name;
+            };
+            return temp;
+        })
 }
 refs.trendContainer.addEventListener('click', onModalFilmCard)
 
@@ -75,7 +75,7 @@ function onModalFilmCard(e) {
             refs.btnAddWatched.addEventListener('click', e => {
                 const watched = JSON.parse(localStorage.getItem('watched')) || [];
                 localStorage.setItem('watched', JSON.stringify(watched))
-                
+
                 if (localStorage.getItem('watched').includes(JSON.stringify(film.id))) {
                     return
                 }
@@ -89,7 +89,7 @@ function onModalFilmCard(e) {
                 const queue = JSON.parse(localStorage.getItem('queue')) || [];
                 localStorage.setItem('queue', JSON.stringify(queue))
 
-                if(localStorage.getItem('queue').includes(JSON.stringify(film.id))) {
+                if (localStorage.getItem('queue').includes(JSON.stringify(film.id))) {
                     return
                 }
                 queue.push(film.id);
@@ -104,7 +104,7 @@ function onModalFilmCard(e) {
 
             refs.lightbox.classList.toggle('modal-is-open')
             refs.overlayModal.insertAdjacentHTML('beforeend', markUp)
-        
+
         })
 }
 
@@ -118,162 +118,163 @@ function fetchMovie() {
         })
 }
 
-refs.closeModalBtn.addEventListener('click',onBtnClose)
-function onBtnClose(){
+refs.closeModalBtn.addEventListener('click', onBtnClose)
+function onBtnClose() {
 
 
 
 
 
-refs.trendContainer.addEventListener('click',e => {
-    if(e.target.nodeName !=='IMG'){
-        return
-      }
+    refs.trendContainer.addEventListener('click', e => {
+        if (e.target.nodeName !== 'IMG') {
+            return
+        }
 
-    openCloseModal();
-      
-    const a = e.target.id
-    return fetch(`https://api.themoviedb.org/3/movie/${a}?api_key=44d74a10460e9a32f8546bed31d47780&language=en-US`)
-    .then(r => r.json())
-    .then( film => {
-        console.log(film)
-        console.log(film.id)
-        return film
+        openCloseModal();
+
+        const a = e.target.id
+        return fetch(`https://api.themoviedb.org/3/movie/${a}?api_key=44d74a10460e9a32f8546bed31d47780&language=en-US`)
+            .then(r => r.json())
+            .then(film => {
+                console.log(film)
+                console.log(film.id)
+                return film
+            })
+            .then(film => {
+                const markUp = articleTpl(film);
+
+                // refs.lightbox.classList.toggle('modal-is-open')
+                refs.overlayModal.insertAdjacentHTML('afterbegin', markUp)
+            })
     })
-    .then(film => {
-        const markUp = articleTpl(film);
-
-        // refs.lightbox.classList.toggle('modal-is-open')
-        refs.overlayModal.insertAdjacentHTML('afterbegin',markUp)  
-    })
-})
 
 
-// refs.closeModalBtn.addEventListener('click',onBtnClose)
+    // refs.closeModalBtn.addEventListener('click',onBtnClose)
 
-// function onBtnClose(){
-//     // refs.lightbox.classList.remove('modal-is-open')
-//     refs.overlayModal.removeChild(refs.overlayModal.firstChild)
-//   }
-
-
-//   /////////////////////////
+    // function onBtnClose(){
+    //     // refs.lightbox.classList.remove('modal-is-open')
+    //     refs.overlayModal.removeChild(refs.overlayModal.firstChild)
+    //   }
 
 
-function pressEsc(e) {
-    if (
-      refs.lightbox.classList.contains('modal-is-open') &&
-      e.code === 'Escape'
-    ) {
-      openCloseModal();
+    //   /////////////////////////
+
+
+    function pressEsc(e) {
+        if (
+            refs.lightbox.classList.contains('modal-is-open') &&
+            e.code === 'Escape'
+        ) {
+            openCloseModal();
+        }
     }
-  }
 
 
     function openCloseModal() {
         refs.overlayModal.innerHTML = '';
 
-    refs.lightbox.classList.toggle('modal-is-open');
-  
-    if (refs.lightbox.classList.contains('modal-is-open')) {
-      window.addEventListener('keydown', pressEsc);
-      refs.closeModalBtn.addEventListener('click', openCloseModal);
-      refs.overlayModal.addEventListener('click', onOverlayClick);
-    } else {
-      window.removeEventListener('keydown', pressEsc);
-      refs.closeModalBtn.removeEventListener('click', openCloseModal);
-      refs.overlayModal.removeEventListener('click', onOverlayClick);
+        refs.lightbox.classList.toggle('modal-is-open');
+
+        if (refs.lightbox.classList.contains('modal-is-open')) {
+            window.addEventListener('keydown', pressEsc);
+            refs.closeModalBtn.addEventListener('click', openCloseModal);
+            refs.overlayModal.addEventListener('click', onOverlayClick);
+        } else {
+            window.removeEventListener('keydown', pressEsc);
+            refs.closeModalBtn.removeEventListener('click', openCloseModal);
+            refs.overlayModal.removeEventListener('click', onOverlayClick);
+        }
     }
-  }
 
     function onOverlayClick(e) {
-    if (e.target.closest('.modal-movie-wrapper')) {
-      return;
-    }
+        if (e.target.closest('.modal-movie-wrapper')) {
+            return;
+        }
         openCloseModal();
-}
-refs.overlay.addEventListener('click', evt => {
-   if(evt.currentTarget === evt.target){
-    openCloseModal();
-   }
-})
-
-
-
-///////////////////////начало пагинации 
-const API__KEY = '44d74a10460e9a32f8546bed31d47780';
-const BASE__URL = 'https://api.themoviedb.org/3/discover/';
-let n = 1;
-refs.next.addEventListener('click',e => {
-    console.log(e.target)
-    n += 1;
-     if(n > 500){
-         n--
-         return
-     }
-     btnCreate()
-    return feachMuvie()
-
-})
-
-if(n === 1){
- refs.btnListPage.classList.add('button-list__page--current');
-}
-
-refs.pr.addEventListener('click', e =>{
-    n-=1;
-    if(n === 0){
-        n++
-        return
     }
-    btnCreate()
-    return feachMuvie()
-
-})
-refs.btnList.addEventListener('click', e => {
-
-    
-    if(e.target.nodeName!== 'BUTTON'){
-        return
-    }
-    
-    console.log(e.target.nodeName,e.currentTarget)
-    
-    n = e.target.textContent -0;
-    btnCreate()
-    feachMuvie()
-
-})
-
-function kekw(kuda, n){
-	refs.btnList.insertAdjacentHTML(kuda,`<li class="button-list__item"><button class="button-list__page">${n}</button></li>`)
-}
-function btnCreate(){
-    refs.btnList.innerHTML = '';
-    refs.btnList.insertAdjacentHTML('afterbegin',`<li class="button-list__item button-list__item--curretn"><button class="button-list__page button-list__page--current">${n}</button></li>`)
-    for(let i = 1; i < 3; i++){
-    if(n+i < 501)
-      kekw('beforeend', n+i);
-     if(n-i > 0)
-            kekw('afterbegin', n-i);
-    }
-
-}
-
-function feachMuvie(){
-    fetch(`${BASE__URL}movie?api_key=${API__KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${n}&with_watch_monetization_types=flatrate`)
-    .then(r => r.json())
-    .then( films => {
-        console.log(films)
-        return films
+    refs.overlay.addEventListener('click', evt => {
+        if (evt.currentTarget === evt.target) {
+            openCloseModal();
+        }
     })
-    .then( ({results}) =>{
 
-        const markUp = trendMovieTpl(results);
-        refs.trendContainer.innerHTML = ''
-        refs.trendContainer.insertAdjacentHTML('beforeend',markUp);
+
+
+    ///////////////////////начало пагинации 
+    const API__KEY = '44d74a10460e9a32f8546bed31d47780';
+    const BASE__URL = 'https://api.themoviedb.org/3/discover/';
+    let n = 1;
+    refs.next.addEventListener('click', e => {
+        console.log(e.target)
+        n += 1;
+        if (n > 500) {
+            n--
+            return
+        }
+        btnCreate()
+        return feachMuvie()
+
     })
-} 
+
+    if (n === 1) {
+        refs.btnListPage.classList.add('button-list__page--current');
+    }
+
+    refs.pr.addEventListener('click', e => {
+        n -= 1;
+        if (n === 0) {
+            n++
+            return
+        }
+        btnCreate()
+        return feachMuvie()
+
+    })
+    refs.btnList.addEventListener('click', e => {
+
+
+        if (e.target.nodeName !== 'BUTTON') {
+            return
+        }
+
+        console.log(e.target.nodeName, e.currentTarget)
+
+        n = e.target.textContent - 0;
+        btnCreate()
+        feachMuvie()
+
+    })
+
+    function kekw(kuda, n) {
+        refs.btnList.insertAdjacentHTML(kuda, `<li class="button-list__item"><button class="button-list__page">${n}</button></li>`)
+    }
+    function btnCreate() {
+        refs.btnList.innerHTML = '';
+        refs.btnList.insertAdjacentHTML('afterbegin', `<li class="button-list__item button-list__item--curretn"><button class="button-list__page button-list__page--current">${n}</button></li>`)
+        for (let i = 1; i < 3; i++) {
+            if (n + i < 501)
+                kekw('beforeend', n + i);
+            if (n - i > 0)
+                kekw('afterbegin', n - i);
+        }
+
+    }
+
+    function feachMuvie() {
+        fetch(`${BASE__URL}movie?api_key=${API__KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${n}&with_watch_monetization_types=flatrate`)
+            .then(r => r.json())
+            .then(films => {
+                console.log(films)
+                return films
+            })
+            .then(({ results }) => {
+
+                const markUp = trendMovieTpl(results);
+                refs.trendContainer.innerHTML = ''
+                refs.trendContainer.insertAdjacentHTML('beforeend', markUp);
+            })
+    }
+}
 
 //конец пагинации
 
